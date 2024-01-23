718e1bb7b36190745d35907b8f4edd162bbe8ed2
DavidFelipeAriza04 <105799393+DavidFelipeAriza04@users.noreply.github.com>
2024-01-22 21:17:43 -0500

    API

---
+++ FastAPI/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return ("Hola Mundo")
+
+@app.get("/palabra")
+async def root():
+    return {"palabra":"Hola"}
