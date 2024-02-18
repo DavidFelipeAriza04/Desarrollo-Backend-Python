ab1aa653183adc768625281dfd29e071afcf05bc
DavidFelipeAriza04 <105799393+DavidFelipeAriza04@users.noreply.github.com>
2024-02-18 18:49:19 -0500

    Tarea1

--- Tarea1.py
+++ Tarea1.py
@@ -1,17 +1,95 @@
-class Figura():
+from math import sqrt
+
+
+class Figura:
     def __init__(self) -> None:
         pass
-    
+
     def get_area(self):
         pass
+
     def get_perimetro(self):
         pass
 
-class Triangulo(Figura):
-    tamano_lado=0
-    def __init__(self,tamano_lado) -> None:
+
+class Cuadrado(Figura):
+    lado = 0
+
+    def __init__(self, lado) -> None:
         super().__init__()
-        self.tamano_lado=tamano_lado
+        self.lado = lado
+
     def get_area(self):
-        return super().get_area()
-    
+        return self.lado**2
+
+    def get_perimetro(self):
+        return self.lado * 4
+
+
+class Rectangulo(Figura):
+    lado1 = 0
+    lado2 = 0
+
+    def __init__(self, lado1, lado2) -> None:
+        super().__init__()
+        self.lado1 = lado1
+        self.lado2 = lado2
+
+    def get_area(self):
+        return self.lado1 * self.lado2
+
+    def get_perimetro(self):
+        return self.lado1 * 2 + self.lado2 * 2
+
+
+class Triangulo(Figura):
+    lado1 = 0
+    lado2 = 0
+    lado3 = 0
+
+    def __init__(self, lado1, lado2, lado3) -> None:
+        super().__init__()
+        self.lado1 = lado1
+        self.lado2 = lado2
+        self.lado3 = lado3
+
+    def get_area(self):
+        s = (self.lado1 + self.lado2 + self.lado3)/2
+        area = sqrt(s*(s - self.lado1)*(s - self.lado2)*(s - self.lado3))
+        return area
+
+    def get_perimetro(self):
+        return self.lado1 + self.lado2 + self.lado3
+
+
+def OpcionNoValida():
+    print("Opcion no valida")
+
+
+def OpcionCuadrado():
+    cuadrado = Cuadrado(int(input("Ingrese el tamano lado: ")))
+    print("Area: " + str(cuadrado.get_area()))
+    print("Perimetro: " + str(cuadrado.get_perimetro()))
+
+
+def OpcionRectangulo():
+    rectangulo = Rectangulo(
+        int(input("Ingrese lado 1: ")), int(input("Ingrese lado 2: "))
+    )
+    print("Area: " + str(rectangulo.get_area()))
+    print("Perimetro: " + str(rectangulo.get_perimetro()))
+
+
+def OpcionTriangulo():
+    triangulo = Triangulo(
+        int(input("Ingrese lado 1: ")), int(input("Ingrese lado 2: ")),int(input("Ingrese lado 3: "))
+    )
+    print("Area: " + str(triangulo.get_area()))
+    print("Perimetro: " + str(triangulo.get_perimetro()))
+
+
+print("Seleccione la figura: \n1.Cuadrado\n2.Rectangulo\n3.Triangulo")
+opcion = input()
+switch_figura = {1: OpcionCuadrado, 2: OpcionRectangulo, 3: OpcionTriangulo}
+
+switch_figura.get(int(opcion), OpcionNoValida)()
