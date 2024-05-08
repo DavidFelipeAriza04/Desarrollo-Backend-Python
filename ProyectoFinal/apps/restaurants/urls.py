56b786024f198c5cf530d5c7e1bc69083dfa2bf1
DavidFelipeAriza04 <105799393+DavidFelipeAriza04@users.noreply.github.com>
2024-05-08 16:53:30 -0500

    .

--- ProyectoFinal/apps/restaurants/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from .views import RestaurantsViewSet
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register(r"restaurants", RestaurantsViewSet, basename="restaurantsViewSet")
-urlpatterns = [
-    path("", include(router.urls)),
-]
--- ProyectoFinal/apps/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from .views import UsersViewSet
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register(r"users", UsersViewSet, basename="usersViewSet")
-urlpatterns = [
-    path("", include(router.urls)),
-]
